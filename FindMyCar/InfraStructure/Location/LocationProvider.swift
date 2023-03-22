3ffd6db91f2b2ad862e79f22bc46f971615f7136
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-03-22 19:11:03 +0900

    ✨ DefaultLocationProvider 타입구현 CLLocationManager 프로퍼티 생성 및 Location 요청 메서드 구현

--- FindMyCar/InfraStructure/Location/LocationProvider.swift
+++ FindMyCar/InfraStructure/Location/LocationProvider.swift
@@ -12,3 +12,44 @@ protocol LocationProvider {
 
     func fetchLocation() -> Location
 }
+
+final class DefaultLocationProvider: NSObject {
+
+    private var locationManager: CLLocationManager
+    private var lastLocation: Location = Location(latitude: 0, longitude: 0)
+
+    init(locationManager: CLLocationManager = CLLocationManager()) {
+        self.locationManager = locationManager
+        super.init()
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.delegate = self
+    }
+}
+
+extension DefaultLocationProvider: LocationProvider {
+
+    func fetchLocation() -> Location {
+        locationManager.requestLocation()
+        return lastLocation
+    }
+}
+
+extension DefaultLocationProvider: CLLocationManagerDelegate {
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default:
+            return
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationCoordinate: CLLocationCoordinate2D = manager.location?.coordinate else {
+            return
+        }
+
+        lastLocation = Location(latitude: locationCoordinate.latitude, longitude: locationCoordinate.longitude)
+    }
+}
