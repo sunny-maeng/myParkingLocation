f27afc27204d974d540947caef4a926abd635abd
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-03-27 17:27:55 +0900

    ✨ MapViewModel 타입 구현 - 배터리절약을 위해 locationManager 생성을 InfraStructure계층에서 ViewModel 의 메서드 fetchLocation()으로 이동 - Location 타입 Domain계층으로 이동

---
+++ FindMyCar/Presenter/MainView/ViewModel/MapViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  MapViewModel.swift
+//  FindMyCar
+//
+//  Created by 써니쿠키 on 2023/03/27.
+//
+
+import Foundation
+import CoreLocation
+
+final class MapViewModel: NSObject {
+
+    let parkingLocation: Observable<Location?> = Observable(nil)
+    let error: Observable<String?> = Observable(nil)
+    let parkingAnnotationTitle: String = "CAR"
+
+    override init() {
+        super.init()
+        fetchLocation()
+    }
+
+    func fetchLocation() {
+        let locationManager: CLLocationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.delegate = self
+        locationManager.requestLocation()
+    }
+}
+
+// CLLocationManagerDelegate
+extension MapViewModel: CLLocationManagerDelegate {
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default:
+            return
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locationCoordinate: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+
+        parkingLocation.value = Location(latitude: locationCoordinate.latitude, longitude: locationCoordinate.longitude)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        guard let CLError = error as? CLError else {
+            self.error.value = "위치 정보를 가져올 수 없습니다."
+            return
+        }
+
+        switch CLError.code {
+        case .denied:
+            self.error.value = "위치정보 권한이 없어 위치를 가져올 수 없습니다."
+        case .network:
+            self.error.value = "네크워크 연결을 확인해주세요."
+        case .locationUnknown:
+            self.error.value = "다시 한 번 시도해주세요"
+        default:
+            self.error.value = "위치 정보를 가져올 수 없습니다."
+        }
+    }
+}
