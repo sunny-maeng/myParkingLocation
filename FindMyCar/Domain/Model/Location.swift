ef853fbf6394426185c962689a3dad72de862e6e
sunnyCookie <107384230+sunny-maeng@users.noreply.github.com>
2023-03-30 21:49:46 +0900

    ✨ LocationType추가

--- FindMyCar/Domain/Model/Location.swift
+++ FindMyCar/Domain/Model/Location.swift
@@ -9,13 +9,23 @@ import Foundation
 
 struct Location {
 
+    let locationType: LocationType
     let latitude: Double?
     let longitude: Double?
     let locationImage: Data?
 
-    init(latitude: Double? = nil, longitude: Double? = nil, locationImage: Data? = nil) {
+    init(latitude: Double, longitude: Double) {
+        self.locationType = .coordinate
         self.latitude = latitude
         self.longitude = longitude
+        self.locationImage = nil
+    }
+
+    init?(locationType: LocationType, locationImage: Data) {
+        guard locationType != .coordinate else { return nil }
+        self.locationType = locationType
+        self.latitude = nil
+        self.longitude = nil
         self.locationImage = locationImage
     }
 }
